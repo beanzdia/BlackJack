340f21e17584493dcd18fe5552e800d9087ff4cb
katiethien <75655850+katiethien@users.noreply.github.com>
2020-12-09 21:49:25 -0800

    Update Card.java

--- src/BlackJack/Card.java
+++ src/BlackJack/Card.java
@@ -3,8 +3,10 @@ package BlackJack;
 import javax.swing.*;
 import java.awt.*;
 
+// This program creates a card, which is represented by a suit and a rank
 public class Card {
 
+    // Defines the 4 suits: spades, diamonds, clubs, and hearts
     enum Suit {
         SPADES("spade"), DIAMONDS("diamond"), CLUBS("club"), HEARTS("heart");
         private final String suitLabel;
@@ -18,6 +20,7 @@ public class Card {
         }
     }
 
+    // Defines the 14 ranks, represented by a value (1-11) and a name (ace-king)
     enum Rank {
         TWO(2, "two"), THREE(3, "three"), FOUR(4, "four"), FIVE(5,"five"),
         SIX(6, "six"), SEVEN(7, "seven"), EIGHT (8, "eight"), NINE(9, "nine"),
@@ -49,12 +52,15 @@ public class Card {
     private Rank rank;
     private boolean faceUp;
 
+    // pre: takes in a suit and rank
+    // Creates an instance of card, represented by a suit, rank, and direction of card (face up or face down)
     public Card (Suit suit, Rank rank) {
         this.suit = suit;
         this.rank = rank;
         this.faceUp = true;
     }
 
+    // Gets the image of the card, either face up or face down
     public ImageIcon drawCardImage() {
         if (faceUp == false) {
             return new ImageIcon("src/img/down.jpg");
@@ -62,20 +68,26 @@ public class Card {
         return new ImageIcon("src/img/" + suit.getSuitLabel() + "_" + rank.getRankLabel() + ".jpg");
     }
 
+    // pre: takes in true or false, where true represents faceup
+    // post: defines placement of a card as face up or down
     public void setFaceUp(boolean faceUp) {
         this.faceUp = faceUp;
     }
 
+    // Returns the suit of a card
     public Suit getSuit() {
         return this.suit;
     }
 
+    // Returns the rank (value and name) of a card
     public Rank getRank() {
         return this.rank;
     }
 
+    // Sets the rank of a card
     public void setRank(Rank rank) {this.rank = rank;}
 
+    // Returns a string listing the suit and value
     public String toString() {
         return "Suit: " + suit + " Value: " + rank;
     }
